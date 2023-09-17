77bccb4b7b293149472b80fc26279f238cc542d1
om-bhardwaj26 <119510197+om-bhardwaj26@users.noreply.github.com>
2023-09-17 15:34:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const popupContainer = document.querySelector('.popup-container');
+const okButton = document.getElementById('okButton');
 
+window.onload = function() {
+    popupContainer.style.display = 'flex';
+};
+
+okButton.addEventListener('click', function() {
+    popupContainer.style.display = 'none';
+});
